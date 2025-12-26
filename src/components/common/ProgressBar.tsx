b8a483e045e0073fc7495d1ea4f62f9b0406cbc4
Tuan Hoang <tuan.hmt@outlook.com>
2025-12-26 17:53:44 +0200

    fix nprogress bar

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -2,17 +2,38 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 
+// Configure NProgress with showSpinner enabled
+NProgress.configure({
+  showSpinner: true,
+  minimum: 0.08,
+  easing: "ease",
+  speed: 500,
+  trickle: true,
+  trickleSpeed: 200,
+});
+
 export default function ProgressBar() {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   useEffect(() => {
+    // Start progress bar when route changes
     NProgress.start();
-    NProgress.done();
-  }, [pathname]);
+
+    // Complete progress bar after route change
+    const timer = setTimeout(() => {
+      NProgress.done();
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+      NProgress.done();
+    };
+  }, [pathname, searchParams]);
 
   return null;
 }
