e48db5dc2b364988b77aa7bf9d958e507687eef2
Tuan Hoang <tuan.hmt@outlook.com>
2026-05-02 22:03:57 +0300

    update dark mode styles for Error 404 page link

--- src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
+++ src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
@@ -40,7 +40,7 @@ export default function Error404() {
 
         <Link
           href="/"
-          className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-5 py-3.5 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
+          className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-5 py-3.5 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/3 dark:hover:text-gray-200"
         >
           Back to Home Page
         </Link>
