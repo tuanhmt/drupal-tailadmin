c85c4dd91270cd8287a7b10faa565e8c14cdb60c
Tuan Hoang <tuan.hmt@outlook.com>
2026-05-02 22:58:17 +0300

    remove unused admin entity pages and API routes for articles and generic entity handling

--- src/lib/drupal.ts
+++ src/lib/drupal.ts
@@ -4,6 +4,4 @@ import { NextDrupal } from "next-drupal";
  * Default Drupal client (unauthenticated)
  * Use for public requests only
  */
-export const drupal = new NextDrupal(
-  process.env.NEXT_PUBLIC_DRUPAL_BASE_URL!, {}
-);
+export const drupal = new NextDrupal(process.env.NEXT_PUBLIC_DRUPAL_BASE_URL || "https://drupal-nextjs.ddev.site");
