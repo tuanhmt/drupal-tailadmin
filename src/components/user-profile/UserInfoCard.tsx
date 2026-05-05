b89ea57a23b3a3defd557f617af27266526ee97c
Tuan Hoang <tuan.hmt@outlook.com>
2026-05-05 13:15:05 +0300

    update UserInfoCard button styles for improved dark mode support

--- src/components/user-profile/UserInfoCard.tsx
+++ src/components/user-profile/UserInfoCard.tsx
@@ -98,7 +98,7 @@ export default function UserInfoCard({ user }: UserInfoCardProps) {
 
         <button
           onClick={openModal}
-          className="flex w-full items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200 lg:inline-flex lg:w-auto"
+          className="flex w-full items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/3 dark:hover:text-gray-200 lg:inline-flex lg:w-auto"
         >
           <svg
             className="fill-current"
